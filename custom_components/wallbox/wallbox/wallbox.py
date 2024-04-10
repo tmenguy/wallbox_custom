e4549af75a3ebe5a9dc4aa3235520a3ce4e3abb8
tmenguy <thomas.menguy@gmail.com>
2024-04-10 15:47:46 +0200

    traces

--- custom_components/wallbox/wallbox/wallbox.py
+++ custom_components/wallbox/wallbox/wallbox.py
@@ -3,21 +3,21 @@
 Wallbox class
 
 """
-
 from datetime import datetime
-from time import timezone
 from requests.auth import HTTPBasicAuth
 import requests
 import json
+import bearerauth
 
-from .bearerauth import BearerAuth
 
+DEFAULT_TIMEOUT_S = 5
+RETRY_ON_TIMEOUT_NUMBER = 3
 
 class Wallbox:
-    def __init__(self, username, password, requestGetTimeout = None, jwtTokenDrift = 0):
+    def __init__(self, username, password, requestGetTimeout = DEFAULT_TIMEOUT_S, jwtTokenDrift = 0):
         self.username = username
         self.password = password
-        self._requestGetTimeout = requestGetTimeout
+        self._requestTimeout = requestGetTimeout
         self.baseUrl = "https://api.wall-box.com/"
         self.authUrl = "https://user-api.wall-box.com/"
         self.jwtTokenDrift = jwtTokenDrift
@@ -30,10 +30,11 @@ class Wallbox:
             "Content-Type": "application/json;charset=UTF-8",
             "User-Agent": "HomeAssistantWallboxPlugin/1.0.0",
         }
+        self._num_retry = RETRY_ON_TIMEOUT_NUMBER
 
     @property
     def requestGetTimeout(self):
-        return self._requestGetTimeout
+        return self._requestTimeout
 
     def authenticate(self):
         auth_path = "users/signin"
@@ -49,14 +50,14 @@ class Wallbox:
                   > datetime.timestamp(datetime.now())):
                 # try to refresh token
                 auth_path = "users/refresh-token"
-                auth = BearerAuth(self.jwtRefreshToken)
+                auth = bearerauth.BearerAuth(self.jwtRefreshToken)
 
         try:
             response = requests.get(
                 f"{self.authUrl}{auth_path}",
                 auth=auth,
                 headers={'Partner': 'wallbox'},
-                timeout=self._requestGetTimeout
+                timeout=self._requestTimeout
             )
             response.raise_for_status()
         except requests.exceptions.HTTPError as err:
@@ -68,125 +69,85 @@ class Wallbox:
         self.jwtRefreshTokenTtl = json.loads(response.text)["data"]["attributes"]["refresh_token_ttl"]
         self.headers["Authorization"] = f"Bearer {self.jwtToken}"
 
+
+
+
+    def _request_method_helper(self, method, url, **kwargs):
+
+        for i in range(self._num_retry):
+            try:
+                response = method(
+                    url,
+                    headers=self.headers,
+                    timeout=self._requestTimeout,
+                    **kwargs
+                )
+                response.raise_for_status()
+                return response
+
+            except requests.exceptions.Timeout as err:
+                # Ok we can continue trying it is a timeout
+                if i >= self._num_retry - 1:
+                    raise (err)
+            except requests.exceptions.HTTPError as err:
+                #has been raised for HTTP errors only shoud trace it
+                raise (err)
+            except requests.exceptions.RequestException as err:
+                #Any other exception from requests (Connection errors, Too many redirects, etc
+                raise (err)
+
+
+    def _get_helper(self, url, **kwargs):
+        return self._request_method_helper(method=requests.get, url=url, **kwargs)
+
+    def _put_helper(self, url, **kwargs):
+        return self._request_method_helper(method=requests.put, url=url, **kwargs)
+
+    def _post_helper(self, url, **kwargs):
+        return self._request_method_helper(method=requests.post, url=url, **kwargs)
+
+
     def getChargersList(self):
         chargerIds = []
-        try:
-            response = requests.get(
-                f"{self.baseUrl}v3/chargers/groups", headers=self.headers,
-                timeout=self._requestGetTimeout
-            )
-            response.raise_for_status()
-        except requests.exceptions.HTTPError as err:
-            raise (err)
+        response = self._get_helper(f"{self.baseUrl}v3/chargers/groups")
         for group in json.loads(response.text)["result"]["groups"]:
             for charger in group["chargers"]:
                 chargerIds.append(charger["id"])
         return chargerIds
 
     def getChargerStatus(self, chargerId):
-        try:
-            response = requests.get(
-                f"{self.baseUrl}chargers/status/{chargerId}", headers=self.headers,
-                timeout=self._requestGetTimeout
-            )
-            response.raise_for_status()
-        except requests.exceptions.HTTPError as err:
-            raise (err)
+        response = self._get_helper(f"{self.baseUrl}chargers/status/{chargerId}")
         return json.loads(response.text)
 
     def unlockCharger(self, chargerId):
-        try:
-            response = requests.put(
-                f"{self.baseUrl}v2/charger/{chargerId}",
-                headers=self.headers,
-                data='{"locked":0}',
-            )
-            response.raise_for_status()
-        except requests.exceptions.HTTPError as err:
-            raise (err)
+        response = self._put_helper(url=f"{self.baseUrl}v2/charger/{chargerId}", data='{"locked":0}')
         return json.loads(response.text)
 
     def lockCharger(self, chargerId):
-        try:
-            response = requests.put(
-                f"{self.baseUrl}v2/charger/{chargerId}",
-                headers=self.headers,
-                data='{"locked":1}',
-            )
-            response.raise_for_status()
-        except requests.exceptions.HTTPError as err:
-            raise (err)
+        response = self._put_helper(url=f"{self.baseUrl}v2/charger/{chargerId}", data='{"locked":1}')
         return json.loads(response.text)
 
     def setMaxChargingCurrent(self, chargerId, newMaxChargingCurrentValue):
-        try:
-            response = requests.put(
-                f"{self.baseUrl}v2/charger/{chargerId}",
-                headers=self.headers,
-                data=f'{{ "maxChargingCurrent":{newMaxChargingCurrentValue}}}',
-            )
-            response.raise_for_status()
-        except requests.exceptions.HTTPError as err:
-            raise (err)
+        response = self._put_helper(url=f"{self.baseUrl}v2/charger/{chargerId}", data=f'{{ "maxChargingCurrent":{newMaxChargingCurrentValue}}}')
         return json.loads(response.text)
 
     def pauseChargingSession(self, chargerId):
-        try:
-            response = requests.post(
-                f"{self.baseUrl}v3/chargers/{chargerId}/remote-action",
-                headers=self.headers,
-                data='{"action":2}',
-            )
-            response.raise_for_status()
-        except requests.exceptions.HTTPError as err:
-            raise (err)
+        response = self._post_helper(url=f"{self.baseUrl}v3/chargers/{chargerId}/remote-action", data='{"action":2}')
         return json.loads(response.text)
 
     def resumeChargingSession(self, chargerId):
-        try:
-            response = requests.post(
-                f"{self.baseUrl}v3/chargers/{chargerId}/remote-action",
-                headers=self.headers,
-                data='{"action":1}',
-            )
-            response.raise_for_status()
-        except requests.exceptions.HTTPError as err:
-            raise (err)
+        response = self._post_helper(url=f"{self.baseUrl}v3/chargers/{chargerId}/remote-action", data='{"action":1}')
         return json.loads(response.text)
 
     def restartCharger(self, chargerId):
-        try:
-            response = requests.post(
-                f"{self.baseUrl}v3/chargers/{chargerId}/remote-action",
-                headers=self.headers,
-                data='{"action":3}',
-            )
-            response.raise_for_status()
-        except requests.exceptions.HTTPError as err:
-            raise (err)
+        response = self._post_helper(url=f"{self.baseUrl}v3/chargers/{chargerId}/remote-action", data='{"action":3}')
         return json.loads(response.text)
 
     def getSessionList(self, chargerId, startDate, endDate):
-        try:
-            payload = {'charger': chargerId, 'start_date': startDate.timestamp(), 'end_date': endDate.timestamp() }
-
-            response = requests.get(
-                f"{self.baseUrl}v4/sessions/stats", params=payload, headers=self.headers,
-                timeout=self._requestGetTimeout
-            )
-            response.raise_for_status()
-        except requests.exceptions.HTTPError as err:
-            raise (err)
+        payload = {'charger': chargerId, 'start_date': startDate.timestamp(), 'end_date': endDate.timestamp()}
+        response = self._get_helper(url=f"{self.baseUrl}v4/sessions/stats", params=payload)
         return json.loads(response.text)
 
     def setEnergyCost(self, chargerId, energyCost):
-        try:
-            response = requests.post(
-                f"{self.baseUrl}chargers/config/{chargerId}",
-                headers=self.headers,
-                json={'energyCost': energyCost},
-            )
-            response.raise_for_status()
-        except requests.exceptions.HTTPError as err:
-            raise (err)
+        response = self._post_helper(url=f"{self.baseUrl}chargers/config/{chargerId}", json={'energyCost': energyCost})
         return json.loads(response.text)
